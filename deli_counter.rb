fb488a32f7ed9114b58349934791d49779343ab2
rtyenriques <tooshopboys@gmail.com>
2021-01-17 22:24:04 -0800

    commit tree 1c71c75acc12a917311fe67e760b0c6c670229ad

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,27 +1,26 @@
-# Write your code here.
+def line(customer)
+  
+  if customer.empty?
+    puts "The line is currently empty."
+  else 
+   current_line = "The line is currently:"
+  customer.each.with_index(1) do |person, ind|
+    current_line << " #{ind}. #{person}"
+  end
+  puts current_line
+end
+end
 
-def line(deli)
-    if deli.empty?
-      puts "The line is currently empty."
-    else
-      current_line = "The line is currently:"
-      deli.each.with_index(1) do |person, i|
-        current_line << " #{i}. #{person}"
-      end
-      puts current_line
-    end
+def take_a_number(number, name)
+  number << name
+  puts "Welcome, #{name}. You are number #{number.length} in line."
+end
+
+def now_serving(number)
+  if number.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{number.first}."
+    number.shift
   end
-  
-  def take_a_number(deli, name)
-    deli << name
-    puts "Welcome, #{name}. You are number #{deli.length} in line."
-  end
-  
-  def now_serving(deli)
-    if deli.empty?
-      puts "There is nobody waiting to be served!"
-    else
-      puts "Currently serving #{deli.first}."
-      deli.shift
-    end
-  end
+end
